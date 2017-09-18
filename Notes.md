09a4d6739acddd79d366cfaf24c96aea783c1ef7
ClosedSequence <nathanh722@gmail.com>
2017-09-17 17:02:11 -0700

    Revert "Revert "Created folters & put existing files into the repo""
    This reverts commit f09beeaf6eb31340c23501b0743d6772943858a5.

---
+++ Notes.md
@@ -0,0 +1,7 @@
+**BPM**
+ - 118
+#TODO
+[✓] Display text in center.
+- JS
+  [ ] Set loop based on BPM
+  [ ] Change color every cycle, alternate
---
+++ js/musicmanager.js
@@ -0,0 +1,14 @@
+/*
+
+███    ██  █████
+████   ██ ██   ██
+██ ██  ██  █████
+██  ██ ██ ██   ██
+██   ████  █████
+
+*/
+var thriller= new Audio("assets/thriller.mp3")
+thriller.loop=true
+$(document).ready(function() {
+  thriller.play();
+});
